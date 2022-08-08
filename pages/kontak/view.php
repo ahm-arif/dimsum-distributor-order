e463f9b4cb7f725d94335e4d2edd5f6e7ad33fd9
aarif <ahmad.arif@verint.com>
2022-08-08 09:25:12 +0700

    Initial Commit - Dimsum Order template

---
+++ pages/kontak/view.php
@@ -0,0 +1,39 @@
+<!-- Page Heading/Breadcrumbs -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="row">
+            <div class="col-lg-12">
+                <h3 class="page-header">
+                    <i style="margin-right:6px" class="fa fa-phone-square"></i>
+                    Kontak
+                </h3>
+                <ol class="breadcrumb">
+                    <li><a href="?page=home">Beranda</a></li>
+                    <li class="active">Kontak</li>
+                </ol>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div style="padding: 0 10px;text-align:justify"> 
+                    <div class="map-responsive">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63452.82399068887!2d106.75533209376849!3d-6.289801284544589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1c333aa0d33%3A0x9f3028e18a6a02ea!2sCilandak%2C%20South%20Jakarta%20City%2C%20Jakarta!5e0!3m2!1sen!2sid!4v1659657082196!5m2!1sen!2sid" 
+                            width="450" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="rows">
+            <div class="col-lg-12">
+                <div style="padding: 0 10px;text-align:justify"> 
+                    <h4>Kantor</h4>
+                    <p><i style="margin-right:10px" class="fa fa-map-marker"></i>Jalan Sementara, Cilandak, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12450</p>
+                    <p><i style="margin-right:10px" class="fa fa-phone"></i>+62 812 1000 386</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /.row -->
