d02740fe756af297d498bab0e17d729a56cab951
aarif <ahmad.arif@verint.com>
2022-08-08 11:16:00 +0700

    Update the DB connection to d3v.site

--- config/database.php
+++ config/database.php
@@ -1,9 +1,13 @@
 <?php
-// deklarasi parameter koneksi database
-$server   = "localhost";
-$username = "root";
-$password = "";
-$database = "pbarang";
+// $server   = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "pbarang";
+
+$server   = "dimsum-order.d3v.site";
+$username = "u1561199_dimsum_order";
+$password = "RIParip_69";
+$database = "u1561199_dimsum_order";
 
 // koneksi database
 $mysqli = new mysqli($server, $username, $password, $database);
