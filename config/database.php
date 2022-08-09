bd83a0a85f259f0926a31251b18a274a2da84ad3
aarif <ahmad.arif@verint.com>
2022-08-10 00:42:21 +0700

    Change the database to d3v.site instead of localhost

--- config/database.php
+++ config/database.php
@@ -1,13 +1,14 @@
 <?php
-$server   = "localhost";
-$username = "root";
-$password = "";
-$database = "pbarang";
+// //### LOCAL ENVIRONMENT ###
+// $server   = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "pbarang";
 
-// $server   = "dimsum-order.d3v.site";
-// $username = "u1561199_dimsum_order";
-// $password = "RIParip_69";
-// $database = "u1561199_dimsum_order";
+$server   = "dimsum-order.d3v.site";
+$username = "u1561199_dimsum_order";
+$password = "RIParip_69";
+$database = "u1561199_dimsum_order";
 
 // koneksi database
 $mysqli = new mysqli($server, $username, $password, $database);
